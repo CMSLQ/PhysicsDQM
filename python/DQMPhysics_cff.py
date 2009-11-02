fbc949129d1672250540ff592c3a1d08a200e954
lockner <>
2009-11-02 22:21:21 +0000

    DQM proccesses

---
+++ python/DQMPhysics_cff.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+## from DQM.Physics.bphysicsOniaDQM_cfi import *
+## from DQM.Physics.ewkDQM_cfi import *
+## from DQM.Physics.ewkWMuNuDQM_cfi import *
+## from DQM.Physics.qcdPhotonsDQM_cfi import *
+## from DQM.Physics.qcdHighPtDQM_cfi import *
+## from DQM.Physics.topDiLeptonDQM_cfi import *
+from Leptoquarks.PhysicsDQM.lqeDQM_cfi import *
+
+dqmPhysics = cms.Sequence(
+##                          bphysicsOniaDQM*
+## 			 ewkDQM*
+##                          ewkWMuNuDQM*
+## 			 qcdPhotonsDQM*
+##                          qcdHighPtDQM*
+## 			 topDiLeptonDQM*
+                         lqeDQM
+			 )
+
